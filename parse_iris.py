eef402f2190ee9988a9dba16886655a1c0de1f93
TabarakoAkula <mishabor2367@gmail.com>
2025-07-13 01:08:10 +0300

    Add IRIS parse

---
+++ parse_iris.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+
+url = "https://www.slovar.co.il/txajax.php"
+
+
+def get_translation(word: str) -> list:
+    output = []
+    response = requests.post(url, data={
+        "xajax": "XTranslate",
+        "xajaxargs[]": f"<xjxquery><q>word={quote(word)}&word_enc=&lang=1&nikud=1&transcription=1</q></xjxquery>"
+    })
+    xml_soup = BeautifulSoup(response.text, "xml")
+    html_soup = BeautifulSoup(xml_soup.findAll("cmd")[-1].text, "html.parser")
+    divs = html_soup.findAll("div", class_=lambda x: x and ("cycle1" in x or "cycle-1" in x))
+    for pare in divs:
+        translation = pare.find("span", "translation")
+        if not translation:
+            continue
+        label = pare.find("span", "word").text
+        translit = pare.find("span", "translit").text
+        translation = translation.text.capitalize()
+        output.append({
+            "word": label,
+            "transcription": translit,
+            "translation": translation})
+    return output
